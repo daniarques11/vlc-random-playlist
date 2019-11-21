42e5369ce73ec78af57ba4f491a69ebf074e0e5c
unknown <darques@cifpfbmoll.eu>
2019-11-21 20:02:00 +0100

    style: Refactorizar

--- parseadorXML.py
+++ parseadorXML.py
@@ -1,10 +1,10 @@
 import xml.etree.ElementTree as ET
+
+
 def parser(library):
     tree = ET.parse(library)
     root = tree.getroot()
-    tracksDic= {}
-
-
+    tracksDic = {}
     for library in root.iter("library"):
         for tracks in library:
             for track in tracks.findall("track"):
