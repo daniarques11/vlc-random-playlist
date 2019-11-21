7117aade19700ac1eb7841b1aeebf0606d36ffb6
DanyZurita <dzurita@cifpfbmoll.eu>
2019-11-21 19:28:16 +0100

    style(parseador): Formatear espacios

--- parseadorXML.py
+++ parseadorXML.py
@@ -1,9 +1,10 @@
 import xml.etree.ElementTree as ET
+
+
 def parser(library):
     tree = ET.parse(library)
     root = tree.getroot()
     tracksDic= {}
-
 
     for library in root.iter("library"):
         for tracks in library:
