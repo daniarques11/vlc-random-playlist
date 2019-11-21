43ab5710736da57c05f74318a851692ba1b96751
unknown <darques@cifpfbmoll.eu>
2019-11-21 20:10:10 +0100

    style: Formatear espacios

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ from parseadorXML import parser
 from cancionesRandom import cancionesRandom
 import os
 
+
 def getListaRutasRandom():
     dicioRutas = parser("libraryP.xml")
     listaRandom = cancionesRandom(dicioRutas)
     return listaRandom
 
+
 def convertirListaAString(lista):
     return " ".join(str(elemento) for elemento in lista)
+
 
 print(convertirListaAString(getListaRutasRandom()))
--- parseadorXML.py
+++ parseadorXML.py
@@ -1,16 +1,13 @@
-<<<<<<< HEAD
-=======
 import xml.etree.ElementTree as ET
 
 
 def parser(library):
     tree = ET.parse(library)
     root = tree.getroot()
-    tracksDic= {}
+    tracksDic = {}
 
     for library in root.iter("library"):
         for tracks in library:
             for track in tracks.findall("track"):
                 tracksDic[track.attrib['ruta']] = track.attrib['id']
     return tracksDic
->>>>>>> f9fe5bec4b589332e443a9e8175e6d038c3d0d76
