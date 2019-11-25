984f6610dfebc2b2196f532711610e8344c3c2c5
unknown <darques@cifpfbmoll.eu>
2019-11-25 17:07:08 +0100

    feat: Ejecutar vlc con canciones
    Convertir lista con rutas de canciones a un solo string con comillas y
    crear un string que ejecute vlc con parametros

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
 from parseadorXML import parser
 from cancionesRandom import cancionesRandom
 import os
+import subprocess
 
 
-def getListaRutasRandom():
-    dicioRutas = parser("libraryP.xml")
+def getListaRutasRandom(libreria):
+    dicioRutas = parser(libreria)
     listaRandom = cancionesRandom(dicioRutas)
     return listaRandom
 
+
+def convertirRutasAString(lista):
+    stringResultado = ''
+    for elemento in lista:
+        stringResultado = stringResultado + '"' + elemento + '" '
+    return stringResultado
+
+def ejecutarVLC(vlcRuta, stringRutas):
+    ejecutable = vlcRuta + " " + stringRutas
+    print(ejecutable)
+    os.popen(ejecutable)
+
+vlcRuta = '"C:\\Program Files (x86)\\VideoLAN\\VLC\\vlc"'
+libreria = "libraryP.xml"
+listaRutas = getListaRutasRandom(libreria)
+stringRutas = convertirRutasAString(listaRutas)
+ejecutarVLC(vlcRuta, stringRutas)
