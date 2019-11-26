2c6da9b6c8a8830676bf9bb124413a91608a7ee5
unknown <darques@cifpfbmoll.eu>
2019-11-26 19:50:59 +0100

    feat: Añadir barricada al acceso a datos

--- parseadorXML.py
+++ parseadorXML.py
@@ -1,13 +1,23 @@
 import xml.etree.ElementTree as ET
+import types
 
 
 def parsear(library):
-    tree = ET.parse(library)
-    root = tree.getroot()
-    tracksDic = {}
+    try:
+        tree = ET.parse(library)
+    except FileNotFoundError:
+        print("No se ha encontrado el archivo " + library +
+              ", comprueba que la ruta del archivo coincide con la ruta de la variable libreria en main.py")
+        exit()
+    except:
+        print("La variable libreria en main.py no es un archivo XML")
+        exit()
+    else:
+        root = tree.getroot()
+        tracksDic = {}
 
-    for library in root.iter("library"):
-        for tracks in library:
-            for track in tracks.findall("track"):
-                tracksDic[track.attrib['ruta']] = track.attrib['id']
-    return tracksDic
+        for library in root.iter("library"):
+            for tracks in library:
+                for track in tracks.findall("track"):
+                    tracksDic[track.attrib['ruta']] = track.attrib['id']
+        return tracksDic
