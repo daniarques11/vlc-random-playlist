e1687305a6a491a8b6a16ddec79d5180a89b9b2e
DanyZurita <dzurita@cifpfbmoll.eu>
2019-11-28 19:11:07 +0100

    refactor(Refactor): Refactorizar

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from servicios import *
 
-
-vlcRuta = '"C:/Program Files (x86)/VideoLAN/VLC/vlc"'
-libreria = "libraryP.xml"
-listaRutas = getListaRutasRandom(libreria)
-stringRutas = getStringRutas(listaRutas)
-ejecutarVLC(vlcRuta, stringRutas)
+def main():
+    vlcRuta = '"C:/Program Files (x86)/VideoLAN/VLC/vlc"'
+    libreria = "libraryP.xml"
+    listaRutas = getListaRutasRandom(libreria)
+    stringRutas = getStringRutas(listaRutas)
+    ejecutarVLC(vlcRuta, stringRutas)
