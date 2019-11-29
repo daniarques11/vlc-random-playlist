5f5e70a0bed7342a84bbcf45023af7f94219b729
DanyZurita <dzurita@cifpfbmoll.eu>
2019-11-29 17:16:32 +0100

    docs: Documentacion

--- test_parseadorXML.py
+++ test_parseadorXML.py
@@ -1,5 +1,6 @@
-from parseadorXML import getDiccionarioRutas as getdic
+from parseadorXML import getDiccionarioRutas as getDic
+import pytest
 
 
-def test_correctoXML():
-    assert getdic("libraryPP.xml") == exit()
+def test_fileNotFoundXML():
+    assert pytest.raises(FileNotFoundError, getDic("libraryPP.xml"))
